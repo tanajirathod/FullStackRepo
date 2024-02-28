6620b5ec1e291b191ab0b11ac9f896a069be4aba
TANAJI RATHOD <TANAJI RATHOD@DESKTOP-FODFNJ9>
2024-02-29 02:28:36 +0530

    TL--B2--task2

--- GITProj-01/src/main/java/com/nt/reports/SalesReport.java
+++ GITProj-01/src/main/java/com/nt/reports/SalesReport.java
@@ -1,8 +1,13 @@
 package com.nt.reports;
 
 public class SalesReport {
+	
 	public void weeklyReport() {
 		System.out.println("SalesReport.weeklyReport()");
+	}
+	
+	public void monthlyReport() {
+		System.out.println("SalesReport.monthlyReport()");
 	}
 
 }
