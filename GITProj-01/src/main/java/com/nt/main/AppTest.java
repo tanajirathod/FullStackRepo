434d5a2205e96179fdae166af85885af7009ab42
TANAJI RATHOD <TANAJI RATHOD@DESKTOP-FODFNJ9>
2024-02-29 01:02:44 +0530

    dev1--conflict-error

--- GITProj-01/src/main/java/com/nt/main/AppTest.java
+++ GITProj-01/src/main/java/com/nt/main/AppTest.java
@@ -9,6 +9,6 @@ public class AppTest{
 		System.out.println("Sum :"+ar.sum(100,200));
 		System.out.println("Sub :"+ar.sub(100,200));
 		System.out.println("-------------------------------");
-		System.out.println("Mul :"+ar.mul(100, 300));
+		System.out.println("Mul :"+ar.mul(100, 300));//123
 	}
 }
