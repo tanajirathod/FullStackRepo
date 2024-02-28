bda6013b1d5ca5bf543c4dbfbe39889822975937
TANAJI RATHOD <TANAJI RATHOD@DESKTOP-FODFNJ9>
2024-02-29 01:43:52 +0530

    B1--code--by dev1

--- GITProj-01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GITProj-01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -1,5 +1,13 @@
 package com.nt.payment;
 
 public class PaymentOperationsService {
+	
+	public String doCreditCardPaymet() {
+		return "Credit Card Payment is done";
+	}
+	
+	public String doDebitCardPaymet() {
+		return "Debit Card Payment is done";
+	}
 
 }
