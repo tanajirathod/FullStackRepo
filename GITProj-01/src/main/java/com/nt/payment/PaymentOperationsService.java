598f6a2674295d6ec60f86f80112a4cb6fa27cea
TANAJI RATHOD <TANAJI RATHOD@DESKTOP-FODFNJ9>
2024-02-29 01:57:37 +0530

    TL--task3

--- GITProj-01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GITProj-01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -9,5 +9,8 @@ public class PaymentOperationsService {
 	public String doDebitCardPaymet() {
 		return "Debit Card Payment is done";
 	}
+	public String doUPIPaymet() {
+		return "UPI payment is done";
+	}
 
 }
